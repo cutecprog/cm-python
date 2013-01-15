36badfafefba04c7153ccc82e0e5bccbe3847ac4
cutecprog <cutecprog@gmail.com>
2013-01-15 14:22:45 -0700

    Simplied my concept quite a bit. Still more planning to go. The changes currently in the file is just experiementation.

--- cm.py
+++ cm.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python
 
+import csv
+
+data1 = []
+data2 = []
+with open('../Downloads/pvs.csv', 'rb') as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.read(1024))
+        csvfile.seek(0)
+        file1 = csv.reader(csvfile, dialect)
+        for row in file1:
+                data1.append(row)
+with open('../Downloads/candidates-01-02-2013-andrea.csv', 'rb') as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.read(1024))
+        csvfile.seek(0)
+        file2 = csv.reader(csvfile, dialect)
+        for row in file2:
+                data2.append(row)
+print data1
+raw_input()
+print data2
+
 def main():
         pass
         '''file1 = open("pvs.csv", "r")
         file2 = open("other_data.csv", "r")
-        match_col = [0, 0]
         data = []
         data.append(csv_to_list(file1.read()))
         data.append(csv_to_list(file2.read()))
-        row_num = len(data[0])
-        match_key = []
-        for i in range(0, row_num):
-                match_key.append(match(data[0][i'''
+        #trim_data
+        match_key = gen_match_key(data)'''
 
 def col_as_int(col):
         """Convert column label to integer.
@@ -51,48 +68,22 @@ def match(item, match_col):
         """
         pass
 
-def order_col(data, order):
-        """Format columns to given order.
-        
-        >>> order_col([[["name","id"],["John","13"],["Sam","7"]],[["foo","age"],["bar","72"],["bar","47"]]], [[0,1],[1]])
-        [["name","id","age"],["John","13","72],["Sam","7","47"]]
+def gen_match_key(data):
+        """Generate matching row indices in data sets.
         
         """
         pass
 
-def csv_to_list(csv_str, dem = ','):
-        """Convert string in CSV format into a 2d list.
+def order_col(data, order):
+        """Format columns to given order.
         
-        >>> csv_to_list("a1,b1,c1,d1,e1,")
-        [['a1', 'b1', 'c1', 'd1', 'e1', '']]
-        >>> csv_to_list("a2,,c2,d2,,")
-        [['a2', '', 'c2', 'd2', '', '']]
-        >>> csv_to_list(",b3,,d3,,f3")
-        [['', 'b3', '', 'd3', '', 'f3']]
-        >>> csv_to_list(",,,,e4,")
-        [['', '', '', '', 'e4', '']]
-        
-        """
-        lines = csv_str.strip('\n').split('\n')
-        csv_data = []
-        for l in lines:
-                csv_data.append(l.strip('\r').split(dem))
-        return csv_data
-
-def list_to_csv(data, dem = ','):
-        """Convert 2d list into string in CSV format.
-        
-        >>> print list_to_csv([['a1', 'b1', 'c1', 'd1', 'e1', ''],['a2', '', 'c2', 'd2', '', ''],\
-['', 'b3', '', 'd3', '', 'f3'],['', '', '', '', 'e4', '']])
-        a1,b1,c1,d1,e1,
-        a2,,c2,d2,,
-        ,b3,,d3,,f3
-        ,,,,e4,
+        >>> order_col([[["name","id"],["John","13"],["Sam","7"]],[["foo","age"],["bar","72"],["bar","47"]]], [[0,1],[1]])
+        [["name","id","age"],["John","13","72"],["Sam","7","47"]]
         
         """
         pass
 
 if __name__=="__main__":
         import doctest
-        if doctest.testmod().failed == 0:
-                main() 
+        #if doctest.testmod().failed == 0:
+        #        main() 
